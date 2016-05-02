abe39a5db74ade20dd54b8281d52f8f850731739
Marek Jelen <marek@jelen.biz>
2016-05-02 13:48:47 +0200

    Add pod name

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
   <body>
     <div class="box">
       <h1>B</h1>
+      <?php if(isset($_ENV['HOSTNAME'])) { ?><h2><?php echo $_ENV['HOSTNAME'] ?></h2><?php } ?>
       <hr>
       <h1 id="countdown">5</h1>
     </div>
